b27015ff18bca4c8a0042afe79ac9fb361f07d98
rodrigomsrocha <rodrigomtc82@gmail.com>
2021-10-14 17:31:33 -0300

    traveling types component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,19 @@
-import { Flex } from "@chakra-ui/react";
+import Head from "next/head";
+import { Flex, Divider } from "@chakra-ui/react";
 import { HomeBanner } from "../components/HomeBanner";
+import { TravelingTypes } from "../components/TravelingTypes";
 
 export default function Home() {
   return (
-    <Flex direction="column">
-      <HomeBanner />
-    </Flex>
+    <>
+      <Head>
+        <title>World Trip | Home</title>
+      </Head>
+      <Flex direction="column">
+        <HomeBanner />
+        <TravelingTypes />
+        <Divider w="90px" h="2px" bg="gray.900" mx="auto" />
+      </Flex>
+    </>
   );
 }
