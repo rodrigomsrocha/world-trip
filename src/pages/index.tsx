039dd8bc3510d226bcd95321995d46522aa6e964
rodrigomsrocha <rodrigomtc82@gmail.com>
2021-10-14 17:45:14 -0300

    call to action message

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import { Flex, Divider } from "@chakra-ui/react";
+import { Flex, Divider, Text, Stack } from "@chakra-ui/react";
 import { HomeBanner } from "../components/HomeBanner";
 import { TravelingTypes } from "../components/TravelingTypes";
 
@@ -13,6 +13,10 @@ export default function Home() {
         <HomeBanner />
         <TravelingTypes />
         <Divider w="90px" h="2px" bg="gray.900" mx="auto" />
+        <Stack fontWeight="medium" fontSize="4xl" textAlign="center" py="52px">
+          <Text>Vamos nessa?</Text>
+          <Text>Então escolha seu continente</Text>
+        </Stack>
       </Flex>
     </>
   );
